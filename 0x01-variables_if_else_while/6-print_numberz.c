68bb548c2f353fad872049ac9f77e1754c42cd55
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-14 15:13:35 -0700

    Add solution for 6-taks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ *        followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48;
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+}
