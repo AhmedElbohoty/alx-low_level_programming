9790f622c3028dc7a1278570357c70cf25d43e67
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-15 13:05:32 -0700

    Add solution for 11-task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of three-digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int digit_1 = 48;
+        int digit_2 = 49;
+        int digit_3 = 50;
+
+        while (digit_1 <= 57)
+        {
+                while (digit_2 <= 57)
+                {
+                        while (digit_3 <= 57)
+                        {
+                                putchar(digit_1);
+                                putchar(digit_2);
+                                putchar(digit_3);
+                                if (digit_1 != 55 || digit_2 != 56 || digit_3 != 57)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                                
+                                digit_3++;
+                        }
+
+                        digit_2++;
+                        digit_3 = digit_2 + 1;
+                }
+
+                digit_1++;
+                digit_2 = digit_1 + 1;
+                digit_3 = digit_1 + 2;
+        }
+        putchar('\n');
+
+        return (0);
+}
