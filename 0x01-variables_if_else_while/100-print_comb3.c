8a5c086808e01c550702497da41fb7049504765f
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-15 10:38:58 -0700

    Add solution for 100-task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit_1 = 48;
+	int digit_2 = 49;
+
+	while (digit_1 <= 57)
+	{
+		while (digit_2 <= 57)
+		{
+			putchar(digit_1);
+			putchar(digit_2);
+			if (digit_1 != 56 || digit_2 != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			digit_2++;
+ 		}
+                
+		digit_1++;
+		digit_2 = digit_1 + 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
