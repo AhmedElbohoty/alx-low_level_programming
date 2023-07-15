d01c5597932c9bb02c00f00e250b766ac40ec977
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-15 13:11:40 -0700

    Add solution for 11-task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,37 +8,34 @@
 
 int main(void)
 {
-        int digit_1 = 48;
-        int digit_2 = 49;
-        int digit_3 = 50;
+	int digit_1 = 48;
+	int digit_2 = 49;
+	int digit_3 = 50;
 
-        while (digit_1 <= 57)
-        {
-                while (digit_2 <= 57)
-                {
-                        while (digit_3 <= 57)
-                        {
-                                putchar(digit_1);
-                                putchar(digit_2);
-                                putchar(digit_3);
-                                if (digit_1 != 55 || digit_2 != 56 || digit_3 != 57)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                                
-                                digit_3++;
-                        }
+	while (digit_1 <= 57)
+	{
+		while (digit_2 <= 57)
+		{
+ 			while (digit_3 <= 57)
+			{
+				putchar(digit_1);
+				putchar(digit_2);
+				putchar(digit_3);
+				if (digit_1 != 55 || digit_2 != 56 || digit_3 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				digit_3++;
+			}
+			digit_2++;
+			digit_3 = digit_2 + 1;
+		}
+		digit_1++;
+		digit_2 = digit_1 + 1;
+		digit_3 = digit_1 + 2;
+	}
+	putchar('\n');
 
-                        digit_2++;
-                        digit_3 = digit_2 + 1;
-                }
-
-                digit_1++;
-                digit_2 = digit_1 + 1;
-                digit_3 = digit_1 + 2;
-        }
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
