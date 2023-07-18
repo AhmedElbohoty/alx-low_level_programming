b6762ef9221ddf2f2341bb0cc00ae4d30b6c7d19
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 10:06:56 -0700

    fix issue

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - prints '_putchar'
  *
  * Description: Write a program that prints _putchar,
- * 		followed by a new line.
+ *		followed by a new line.
  * Return: Always (0)
  */
 int main(void)
