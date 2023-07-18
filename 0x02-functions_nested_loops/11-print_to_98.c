73000aaffa16ac1d28b84edf5325aba963bb1a8a
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 15:05:48 -0700

    Add solution for '11. 98 Battery Street, the OG' task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * @n: The first number - starting point
+ *
+ * Return: Void 
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	printf("98");
+	printf("\n");
+}
+
