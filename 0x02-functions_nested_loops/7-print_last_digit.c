0ec9aba6390e368eaff3b1da11c0b7d5f6f30e15
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 13:25:03 -0700

    Fix issue

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,11 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		last_digit = (-1 * n) % 10;
+		last_digit = (-1 * (n % 10));
 	}
 	else
 	{
-		last_digit = n % 10;
+		last_digit = (n % 10);
 	}
 
 	_putchar(last_digit + '0');
