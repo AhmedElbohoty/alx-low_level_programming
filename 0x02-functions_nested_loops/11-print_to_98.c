b00b1c940c14077c898d43159fe2b0c7185ffbee
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 15:07:55 -0700

    Fix issue

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 - prints natural numbers from n to 98, followed by a new line
  * @n: The first number - starting point
  *
- * Return: Void 
+ * Return: Void
  */
 void print_to_98(int n)
 {
