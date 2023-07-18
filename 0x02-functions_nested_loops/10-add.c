a4b12f51e7b01932dc6a308f2d02de6b2667f7a1
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 14:49:05 -0700

    Add solution for '10. a + b' task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * Return: the sum result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+
