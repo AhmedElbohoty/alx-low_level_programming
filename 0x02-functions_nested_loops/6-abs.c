0c6f9cf8eefc89be1bed359311a0a4b18d4a671c
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-18 12:46:40 -0700

    Add solution for "There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you" task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The number to be checked
+ *
+ * Return: The absolute value of number
+ */
+int _abs(int n)
+{
+	return n * ((n > 0) - (n < 0));
+}
+
