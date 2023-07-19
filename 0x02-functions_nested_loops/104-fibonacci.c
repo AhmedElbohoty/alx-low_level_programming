190e368415434122d04db7e3ead1c2f51794fc5d
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-19 09:26:45 -0700

    Fix issue

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-	int divisor = 1000000;
+	int divisor = 1000000000;
 	unsigned long i = 1;
 	unsigned long numb_1 = 1;
 	unsigned long numb_2 = 2;
@@ -31,7 +31,7 @@ int main(void)
 	numb_1_r = numb_1 % divisor;
 	numb_2_q = numb_2 / divisor;
 	numb_2_r = numb_2 % divisor;
-	for (i = 92; i < 95; ++i)
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", numb_2_q + (numb_2_r / divisor));
 		printf("%lu", numb_2_r % divisor);
