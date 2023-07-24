7bc1571fcc8214d971c9cccc2ef8c797f0bbcddf
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-24 16:16:50 -0700

    Fix issue

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,15 +8,14 @@
  */
 void _puts(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        /* code */
-        _putchar(s[i]);
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
