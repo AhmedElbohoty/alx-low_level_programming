6d03d360e13b7eea3ac86370c48903f70449f157
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-24 14:50:57 -0700

    Add solution for '0-task'

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ *               the value it points to to 98
+ * @a: the integer to reset
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *a)
+{
+	*a = 98;
+}
+
