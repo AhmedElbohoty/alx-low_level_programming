63d1e435b6875f058a42377bce9d33b599ae08fb
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-24 16:21:43 -0700

    Add solution for "6-task"

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @s: the string to check
+ *
+ * Return: the length of a string.
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	if (i % 2 == 0)
+		_putchar(s[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
