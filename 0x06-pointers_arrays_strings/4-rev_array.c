49196efc811da2573156399acfee7a4e5697b50e
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-26 11:05:58 -0700

    Add solution for '4-task'

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: the pointer to string
+ * @n: the number of elements of the array
+ *
+ * Return: Nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
+
