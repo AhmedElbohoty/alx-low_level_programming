3ccfa9a37cb966d1799234abe06ef580e640fc69
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-26 09:55:31 -0700

    Fix issue

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,33 @@
 #include "main.h"
 
 /**
-* *_strncpy -  concatenates two strings
-* @dest: the first pointer to dest
-* @src: the second pointer to src
-* @n: the n bytes from src
-*
-* Return: the point to dest
-*/
+ * *_strncpy -  concatenates two strings
+ * @dest: the first pointer to dest
+ * @src: the second pointer to src
+ * @n: the n bytes from src
+ *
+ * Return: the point to dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int i = 0;
 
-	if (!dest)
-		return dest;
-
-	while (*src && n--)
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		if (i >= n)
+			break;
+
+		dest[i] = src[i];
+			i++;
 	}
 
-	*dest = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
-	return ptr;
+	return (dest);
 }
 
