fd9ff50eb313fa765d2c1c5454b7e72d756ab5c3
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-31 14:56:34 -0700

    Fix issue

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s > '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
-			break;
-		s++;
+		if (s[i] == c)
+			return (s + i);
+		i++;
 	}
 
 	return (s);
