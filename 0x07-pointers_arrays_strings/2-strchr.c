0c195803dba2a27940541caa5ced5041fc32f7e2
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-31 14:34:26 -0700

    Fix issue

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,12 @@
  * Return: a pointer to the first occurrence of the character c in the
  *         string s, or NULL if the character is not found
  */
-
 char *_strchr(char *s, char c)
 {
 	while (*s >= '\0')
 	{
 		if (*s == c)
-		{
-			s++;
 			break;
-		}
 		s++;
 	}
 
