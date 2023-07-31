ef211100c31a13f91dad24a1a933495b847756ff
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-07-31 10:39:42 -0700

    Fix issue

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,21 +5,18 @@
  * @s: the pointer to string
  * @c: the character
  *
- * Return: a pointer to the first occurrence of the character c
- *         in the string s, or NULL if the character is not found
+ * Return: a pointer to the first occurrence of the character c in the
+ *         string s, or NULL if the character is not found
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\n')
+	while (*s >= '\n')
 	{
 		s++;
 		if (*s == c)
 			break;
 	}
-
-	if (*s != c)
-		s++;
 
 	return (s);
 }
