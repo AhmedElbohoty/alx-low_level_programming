3a681141677e94615c04118d1c5a48ff10248617
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-01 11:48:17 -0700

    Fix issue

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_strchr - locates a character in a string.
- * @s: the pointer to string
- * @c: the character
- *
- * Return: a pointer to the first occurrence of the character c in the
- *         string s, or NULL if the character is not found
- */
+* *_strchr - locates a character in a string.
+* @s: the pointer to string
+* @c: the character
+*
+* Return: a pointer to the first occurrence of the character c in the
+*         string s, or NULL if the character is not found
+*/
 char *_strchr(char *s, char c)
 {
 	char *null_pt = 0;
