a52a9e3b2b892dbb5ca4f1bbbb3902b5a17608a8
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-02 09:50:01 -0700

    Add solution for '0-puts_recursion.c'

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - locates a character in a string.
+ * @s: the string to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
