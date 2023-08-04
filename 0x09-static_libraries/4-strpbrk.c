5291266b1fcd815b246aba24d35605108e4e305e
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-04 13:46:54 -0700

    Add solution for 'libmy.a'

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* *_strchr - locates a character in a string.
+* @s: the pointer to string
+* @c: the character
+*
+* Return: a pointer to the first occurrence of the character c in the
+*         string s, or NULL if the character is not found
+*/
+char *_strchr(char *s, char c)
+{
+	char *null_pt = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+
+	if (*s == '\0')
+		return (null_pt);
+
+	return (s);
+}
+
