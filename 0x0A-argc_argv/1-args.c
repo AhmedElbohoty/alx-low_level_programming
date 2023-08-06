d39aaff3d08fbc423782d4b5d901beaab3dd7d47
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-06 03:11:55 -0700

    Add solution for '1-args.c'

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the count of args
+ * @argc: the count of args
+ * @argv: array of args
+ *
+ * Return: (Always) 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+
+	return (0);
+}
