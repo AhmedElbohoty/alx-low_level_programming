c1b4576977b4d09758c120401a733d81d529a1be
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-09 13:34:38 -0700

    Add file for '101-strtow.c'

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **strtow - splits a string into words
+ * @str: The pointer to string
+ *
+ * Return: Pointer to array (Success)
+ *         NULL (Failure)
+ */
+char **strtow(char *str)
+{
+	int **p;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	return (p);
+}
+
