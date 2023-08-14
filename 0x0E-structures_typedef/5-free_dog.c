3cc596c5371e5e80e85a71d1b21fe55b57733680
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-14 14:11:18 -0700

    Fix issue

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,10 +9,11 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		exit(EXIT_FAILURE);
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
 
