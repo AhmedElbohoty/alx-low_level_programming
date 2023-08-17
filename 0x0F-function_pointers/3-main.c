221f705ac94aae7e18705933bef8371659dd3f2a
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-17 11:02:12 -0700

    Add solution for '3-main.c' task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculation
+ * @argc: count of arguments
+ * @argv: vector arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int operand_1, operand_2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operand_1 = atoi(argv[1]);
+	operator = argv[2];
+	operand_2 = atoi(argv[3]);
+
+	if (get_op_func(operator) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == '/' && operand_2 == 0) ||
+			(*operator == '%' && operand_2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(operand_1, operand_2));
+
+	return (0);
+}
+
