63c5de74523f33db0d052bad770d93c8ff36cfdf
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-17 23:50:55 -0700

    Add solution for 0-sum_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - the sum of all its parameters
+ * @n: count of args
+ *
+ * Retrun: (0) if n eqauls 0
+ *         ELSE returns the sum of all args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args_ptr;
+	unsigned i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args_ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args_ptr, int);
+	}
+
+	va_end(args_ptr);
+
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIADIC_FUNCTION_HEADER
+#define VARIADIC_FUNCTION_HEADER
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
+
