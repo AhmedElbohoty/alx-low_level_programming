1325f6003d9da2c14c4942a9f9363d20ea5260f7
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-24 13:44:54 -0700

    Add solution for '2-add_node.c'

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to pointer of head
+ * @str: string to be added
+ *
+ * Return: the number of nodes.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = NULL;
+
+	temp = malloc(sizeof(list_t));
+	temp->str = strdup(str);
+	temp->len = _strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (*head);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
