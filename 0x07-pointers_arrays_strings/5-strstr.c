3e667473faefd8b306d2926cad23221e3d04045c
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-08-28 02:47:44 -0700

    Add solution for '5-strstr.c'

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,34 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _strstr - locates a substring
- * @haystack: the string
- * @needle: the substring
- *
- * Return: Nothing
- */
+* _strstr - locates a substring
+* @haystack: the string
+* @needle: the substring
+*
+* Return: haystack - if needle is empty
+*         Null     - if needle occurs nowhere in haystack
+*         pointer to the first character of the first occurrence of needle
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	return (haystack);
+	int i;
+
+	if (!needle)
+		return (haystack);
+
+	while (*haystack != '\0')
+	{
+		i = 0;
+		while (needle[i] == haystack[i])
+		{
+			if (needle[i] == '\0')
+				return (haystack);
+			i++;
+		}
+
+		haystack++;
+	}
+	return (NULL);
 }
 
