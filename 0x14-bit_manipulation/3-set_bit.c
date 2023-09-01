947fa26246fee899ce40b199c4a59b0344d56f7b
AhmedElbohoty <a.ramzielbohoty@gmail.com>
2023-09-01 01:49:25 -0700

    Fix issue

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 31)
+	unsigned int size = sizeof(unsigned long int) * 8;
+
+	if (index > size - 1)
 		return (-1);
 
 	*n = (1 << index) | *n;
