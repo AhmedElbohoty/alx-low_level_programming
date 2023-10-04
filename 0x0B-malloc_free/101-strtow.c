83d0ebe88f935b9e72dd27684694961f2c162d64
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-04 13:48:17 +0300

    Implement solution for 0x0B-malloc_free - 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,20 +1,93 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * **strtow - splits a string into words
- * @str: The pointer to string
+ * strtow - splits a string into words.
+ * @str: pointer to the string
  *
- * Return: - If string equals NULL or "", return NULL.
- *  .      - Else,
+ * Return: pointer to Null-terminated array of strings
  */
 char **strtow(char *str)
 {
-	char **p;
+	char **argv = NULL;
+	int str_size = _strlen(str);
+	int arr_size = words_count(str);
+	char prev_char = '\0';
+	int i = 0, j = 0, m = 0;
+	int start;
 
-	if (str == NULL || !str)
-		return (NULL);
+	argv = malloc(sizeof(char *) * (arr_size + 1));
 
-	return (p);
+	for (i = 0; i < str_size; i++)
+	{
+		if (str[i] != ' ')
+		{
+			if (str[i + 1] == ' ')
+			{
+				argv[j] = malloc(sizeof(char) * (i + 1 - start + 1));
+				/* Check for error and free */
+				m = 0;
+				while (start <= i)
+				{
+					argv[j][m] = str[start];
+					m++;
+					start++;
+				}
+
+				argv[j][start] = '\n';
+				j++;
+			}
+			else
+			{
+				if (prev_char == ' ')
+				start = i;
+			}
+		}
+		prev_char = str[i];
+	}
+	argv[j] = NULL;
+
+	return (argv);
+}
+
+/**
+ * words_count - Get the count of tokens
+ * @str: pointer to the string
+ *
+ * Return: count of tokens
+ */
+int words_count(char *str)
+{
+	int count = 0, i;
+	char prev_char = '\0';
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && prev_char == ' ')
+			count++;
+
+		prev_char = str[i];
+	}
+
+	return (count);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (len);
+
+	len = 1 + _strlen(s + 1);
+
+	return (len);
 }
 
