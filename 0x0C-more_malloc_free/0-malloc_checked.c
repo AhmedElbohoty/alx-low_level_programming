1de39dfede73351c48cf3e63e1f1abb573dd5c9c
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-07 18:45:01 +0300

    Implement solution for 0x0C-malloc_checked.c - task 0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the size of mempry to be allocated
+ *
+ * Return: - If it fails, exit with status 98.
+ *         - Else, return pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);
+
+    return (ptr);
+}
