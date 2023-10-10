1f77e20d594ab79b6a942bad972d3b9d72d74e3a
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-10 19:28:33 +0300

    Implement solution for 0x0E-structures_typedef - 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - initialize a variable of type struct dog
+ * @d: pointer to the dog
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (!d->age)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s", d->owner);
+	}
+}
