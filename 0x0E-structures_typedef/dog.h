5d7503977d0bbfb2ceaa8bd3ef3b39091ff748b9
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-10 19:16:11 +0300

    Implement solution for 0x0E-structures_typedef - dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef HEADER_DOG
+#define HEADER_DOG
+
+/**
+ * struct dog - structure to describe dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+
+#endif
+
