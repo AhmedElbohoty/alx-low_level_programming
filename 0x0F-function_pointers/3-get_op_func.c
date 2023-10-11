4aca045c568241cd7915e3b55eaaa58d9eb02553
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-11 21:55:44 +0300

    Add solution for project 0x0F-function_pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* get_op_func - the function pointer to select the correct opertion
+* @s: the pointer to string
+*
+* Return: the correct operation
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (*ops[i].f);
+
+		i++;
+	}
+
+	return (NULL);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Sum of two numbers.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - the difference of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Difference of two numbers.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Product of two numbers.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - result of the division
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Result of the division.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - The remainder of the division
+ * @a: Number
+ * @b: Number
+ *
+ * Return: the remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
