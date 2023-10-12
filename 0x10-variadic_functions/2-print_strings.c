f49f0d5dbf355b500182b4559b9668a01e6ab074
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-10-12 17:24:41 +0300

    Add header filw

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,33 +6,28 @@
 /**
  * print_strings - prints strings
  * @separator: separator between strings
- * @n: number of arguments
+ * @n: count of arguments
  *
  * Return: Nothing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
 	char *str;
+	unsigned int i;
+	va_list ptr;
 
-	va_list args_ptr;
-
-	va_start(args_ptr, n);
-
+	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args_ptr, char *);
+		str = va_arg(ptr, char *);
 		if (str == NULL)
 			str = "(nil)";
 
 		printf("%s", str);
-
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 
-	va_end(args_ptr);
-
 	printf("\n");
+	va_end(ptr);
 }
-
