b3bc44a83ef92797357c777bb5a00c9c388a4885
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-11 16:57:38 +0200

    Add solution for 100-operations.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,60 @@
+/**
+ * add - sum of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Sum of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - the difference of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Difference of two numbers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - product of two numbers
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Product of two numbers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - result of the division
+ * @a: Number
+ * @b: Number
+ *
+ * Return: Result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - The remainder of the division
+ * @a: Number
+ * @b: Number
+ *
+ * Return: the remainder of the division.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
