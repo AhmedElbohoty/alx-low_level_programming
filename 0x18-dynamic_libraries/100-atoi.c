af776b7bf746b7f2414d23b244631f8800e7ecee
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-11 14:10:24 +0200

    Fix isssue

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -40,15 +40,3 @@ int _atoi(char *s)
 	}
 	return (res);
 }
-
-/**
- * _isdigit - checks for a digit (0 through 9).
- * @n: the char to be checked
- *
- * Return: Always 0.
- */
-int _isdigit(int n)
-{
-	return (n >= '0' && n <= '9');
-}
-
