7803df1b91669ca1b19a092aa7dd048bbb9cd072
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2023-12-21 14:46:50 +0200

    clear

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates the hash table.
+ * @size: Size of the table.
+ *
+ * Return: - If size equals 0 or malloc failed, return NULL.
+ *         - Else, retunr the hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht;
+    unsigned long int i = 0;
+
+    if (size == 0)
+        return (NULL);
+
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+
+    ht->size = size;
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+        return (NULL);
+
+    while (i < size)
+    {
+        ht->array[i] = NULL;
+        i++;
+    }
+
+    return (ht);
+}
