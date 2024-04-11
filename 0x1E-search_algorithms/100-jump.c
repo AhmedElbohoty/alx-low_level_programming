cb88e48aca3ea6ccb2f46320718825590054c863
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-11 13:57:03 +0200

    Add solution

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,33 +14,33 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t index, jump, step;
+	size_t index, jump, step;
 
-    /* Check for NULL array or empty size */
-    if (array == NULL || size == 0)
-        return (-1);
+	/* Check for NULL array or empty size */
+	if (array == NULL || size == 0)
+		return (-1);
 
-    /* Calculate the step size */
-    step = sqrt(size);
+	/* Calculate the step size */
+	step = sqrt(size);
 
-    /* Jump search */
-    for (index = jump = 0; jump < size && array[jump] < value;)
-    {
-        printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-        index = jump;
-        jump += step;
-    }
+	/* Jump search */
+	for (index = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		index = jump;
+		jump += step;
+	}
 
-    printf("Value found between indexes [%ld] and [%ld]\n", index, jump);
+	printf("Value found between indexes [%ld] and [%ld]\n", index, jump);
 
-    /* Adjust jump if it's exceeding the array size */
-    jump = jump < size - 1 ? jump : size - 1;
+	/* Adjust jump if it's exceeding the array size */
+	jump = jump < size - 1 ? jump : size - 1;
 
-    /* Linear search  */
-    for (; index < jump && array[index] < value; index++)
-        printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+	/* Linear search  */
+	for (; index < jump && array[index] < value; index++)
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 
-    printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+	printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 
-    return (array[index] == value ? (int)index : -1);
+	return (array[index] == value ? (int)index : -1);
 }
