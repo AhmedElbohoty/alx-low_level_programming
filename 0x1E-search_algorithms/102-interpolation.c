ad5885a20f3bf3dfc1a820a0ff21fd5c497b2d80
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-11 15:11:15 +0200

    Add solution

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,11 +19,16 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
-	while (value >= array[low] && value <= array[high] && low <= high)
+	while (low <= high)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
-				(value - array[low]));
+					 (value - array[low]));
 
+		if (pos >= size)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			break;
+		}
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
