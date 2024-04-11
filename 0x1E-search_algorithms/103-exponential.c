226c03d071a3f77d7c04f5ee52e5c0b59617c88a
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-11 19:08:50 +0200

    Add solution

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -25,7 +25,7 @@ int exponential_search(int *array, size_t size, int value)
 	index = 1;
 	while (index < size && array[index] <= value)
 	{
-		printf("Checked value at index %lu: %d\n", index, array[index]);
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 		index *= 2;
 	}
 
